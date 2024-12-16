67b4b35cec3c0066633429154f5c42c58dd7440c
nguyenuyen55 <nguyenuyen552000@gmail.comm>
2024-12-16 21:46:15 +0700

    add stripe webhook

---
+++ SupperHeroAPI-Dotnet8/Controllers/StripeWebHookController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace SupperHeroAPI_Dotnet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StripeWebHookController : ControllerBase
+    {
+        const string endpointSecret = "whsec_3baae3aa02fe1761b4ce0d585b04205a29db28ff3427c9417ff6456ad4606a0f";
+
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], endpointSecret);
+
+                // Handle the event
+                // If on SDK version < 46, use class Events instead of EventTypes
+                if (stripeEvent.Type == EventTypes.PaymentIntentSucceeded)
+                {
+                    var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                    Console.WriteLine("PaymentIntent was successful!");
+                }
+                else if (stripeEvent.Type == EventTypes.PaymentMethodAttached)
+                {
+                    var paymentMethod = stripeEvent.Data.Object as PaymentMethod;
+                    Console.WriteLine("PaymentMethod was attached to a Customer!");
+                }
+                // ... handle other event types
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
